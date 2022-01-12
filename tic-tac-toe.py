3233ae425cfff82aa44bae146fb4fb49a632b6c6
RedIts <redito.dean04@gmail.com>
2022-01-12 19:55:41 +0800

    Functioning Tic-Tac-Toe game.
    Working game but still has error of putting a player move to an already occupied box.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -4,7 +4,12 @@ Author: Dean Redito
 '''
 
 def main():
+    player = next_player("")
     board = create_board()
+    while not (has_winner(board) or a_draw(board)):
+        display_board(board)
+        player_move(player, board)
+        player = next_player(player)
     display_board(board)
 
 def create_board():
@@ -15,12 +20,40 @@ def create_board():
 
 def display_board(board):
     print()
-    print(f"{board[0]} | {board[1]} | {board[2]}")
-    print(f"- + - + -")
-    print(f"{board[3]} | {board[4]} | {board[5]}")
-    print(f"- + - + -")
-    print(f"{board[6]} | {board[7]} | {board[8]}")
+    print(" ———————————")
+    print(f"| {board[0]} | {board[1]} | {board[2]} |")
+    print(f" —— + — + —— ")
+    print(f"| {board[3]} | {board[4]} | {board[5]} |")
+    print(f" —— + — + —— ")
+    print(f"| {board[6]} | {board[7]} | {board[8]} |")
+    print(" ———————————")
     print()
+
+def player_move(player, board):
+    box = int(input(f"{player}'s turn to play. Choose a square (1-9): "))
+    board[box - 1] = player
+
+def next_player(current):
+    if current == "" or current == "o":
+        return "x"
+    elif current =="x":
+        return "o"
+
+def has_winner(board):
+    return (board[0] == board[1] == board[2] or #Top 1|2|3
+            board[3] == board[4] == board[5] or #Middle 4|5|6
+            board[6] == board[7] == board[8] or #Bottom 7|8|9
+            board[0] == board[3] == board[6] or #Left 1|4|7
+            board[1] == board[4] == board[7] or #Middle 2|5|8
+            board[2] == board[5] == board[8] or #Right 3|6|9
+            board[0] == board[4] == board[8] or #Diagonal 1|5|9
+            board[2] == board[4] == board[6])   #Diagonal 3|5|7
+
+def a_draw(board):
+    for box in range(9):
+        if board[box] != "x" and board[box] != "o":
+            return False
+    return True
 
 if __name__ == "__main__":
     main()
