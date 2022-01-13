929dd6f4570b20c99f7c4cded35365ccaa51be41
RedIts <redito.dean04@gmail.com>
2022-01-13 11:52:46 +0800

    Commit Display Board
    Display board with error on player moves.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,59 +1,67 @@
-'''
+"""
 Tic-Tac-Toe
 Author: Dean Redito
-'''
+"""
 
 def main():
-    player = next_player("")
-    board = create_board()
-    while not (has_winner(board) or a_draw(board)):
-        display_board(board)
-        player_move(player, board)
-        player = next_player(player)
-    display_board(board)
+    curr_player = next_player('')
 
-def create_board():
-    board = []
-    for box in range (9):
-        board.append(box + 1)
-    return board
+    values = [' ' for x in range(9)]
 
-def display_board(board):
+    player_pos = {'X': [], 'O': []}
+
+    while True:
+        print_board(values)
+
+        #Try Exception Block for wrong input on move variable
+        try:
+            print(f"Player {curr_player}'s turn. Choose a spot: ", end="")
+            move = int(input())
+        except ValueError:
+            print("No such moves. Please Try Again.")
+            continue
+
+        #Input range check
+        if move <1 or move >9:
+            print("Values not on Board. Please Try Again.")
+            continue
+
+        #Check spot if not occupied by another player
+        if values[move - 1] != " ":
+            print("Spot taken. Please Try Again.")
+            continue
+
+        #Update board info
+        values[move - 1] = curr_player
+
+        player_pos[curr_player].append(move)
+
+def print_board(values):
+    """
+    Function for displaying board.
+    """
     print()
     print(" ———————————")
-    print(f"| {board[0]} | {board[1]} | {board[2]} |")
+    print(f"| {values[0]} | {values[1]} | {values[2]} |")
     print(f" —— + — + —— ")
-    print(f"| {board[3]} | {board[4]} | {board[5]} |")
+    print(f"| {values[3]} | {values[4]} | {values[5]} |")
     print(f" —— + — + —— ")
-    print(f"| {board[6]} | {board[7]} | {board[8]} |")
+    print(f"| {values[6]} | {values[7]} | {values[8]} |")
     print(" ———————————")
     print()
 
-def player_move(player, board):
-    box = int(input(f"{player}'s turn to play. Choose a square (1-9): "))
-    board[box - 1] = player
+def win_condition(player_pos, curr_player):
+    """
+    Check for win conditions.
+    """
+    #Possible win combinations
+    combinations = [[1,2,3], [4,5,6], [7,8,9,], [1,4,7], [2,5,8], [3,6,9]]
+
 
 def next_player(current):
     if current == "" or current == "o":
         return "x"
-    elif current =="x":
+    elif current == "x":
         return "o"
-
-def has_winner(board):
-    return (board[0] == board[1] == board[2] or #Top 1|2|3
-            board[3] == board[4] == board[5] or #Middle 4|5|6
-            board[6] == board[7] == board[8] or #Bottom 7|8|9
-            board[0] == board[3] == board[6] or #Left 1|4|7
-            board[1] == board[4] == board[7] or #Middle 2|5|8
-            board[2] == board[5] == board[8] or #Right 3|6|9
-            board[0] == board[4] == board[8] or #Diagonal 1|5|9
-            board[2] == board[4] == board[6])   #Diagonal 3|5|7
-
-def a_draw(board):
-    for box in range(9):
-        if board[box] != "x" and board[box] != "o":
-            return False
-    return True
-
 if __name__ == "__main__":
     main()
